83e116d210654af26487fb3eaefb0df82b5501a6
krish <krishnanprasad.93@gmail.com>
2024-12-11 23:45:12 +0530

    test for devops azure

--- edpicker-api/WeatherForecast.cs
+++ edpicker-api/WeatherForecast.cs
@@ -7,7 +7,6 @@ namespace edpicker_api
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
     }
 }
